ed30aa3b61d4746e0ddbd1df62f15958ef4d84d5
njueyupeng <1196194875@qq.com>
2020-06-21 23:58:09 +0800

    初始化项目文件

---
+++ src/views/person/components/messageBoard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :xs="12" :sm="12" :lg="6" v-for="(item,index) in messageList" :key="index">
+        <person-card :message="item" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import personCard from "./personCard";
+export default {
+  components: { personCard },
+  data() {
+    return {
+      messageList: [
+        {
+          title: "今日日志",
+          info: [
+            {
+              num: 102400,
+              text: "Log today"
+            }
+          ]
+        },
+        {
+          title: "今日活跃",
+          info: [
+            {
+              num: 800,
+              text: "active today"
+            }
+          ]
+        },
+        {
+          title: "人员统计",
+          info: [
+            {
+              num: 1800,
+              text: "在校人数"
+            },
+            {
+              num: 900,
+              text: "其他人数"
+            }
+          ]
+        },
+        {
+          title: "日志统计",
+          info: [
+            {
+              num: 36,
+              text: "日志数量"
+            },
+            {
+              num: 12,
+              text: "学生记录数"
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/person/components/personCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="person-card">
+    <div class="header">
+      <span class="title">{{message.title}}</span>
+      <span class="badage">{{message.badage||'Today'}}</span>
+    </div>
+    <div class="container">
+      <div v-for="(info,index) in message.info" :key="index" style="width:45%">
+        <count-to :start-val="0" :end-val="info.num" :duration="1600" class="card-panel-num" />
+        <div class="card-panel-text">{{info.text}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CountTo from "vue-count-to";
+export default {
+  name: "personCard",
+  components: { CountTo },
+  props: {
+    message: {
+      default: () => ({
+        title: "",
+        badage: "",
+        info: []
+      }),
+      type: Object
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.person-card {
+  min-height: 50px;
+  font-size: 12px;
+  border-radius: 4px;
+  background-color: #fff;
+  overflow: hidden;
+  box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.05);
+  border-color: rgba(0, 0, 0, 0.05);
+  &:hover {
+    box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.2);
+  }
+  margin-bottom: 16px;
+  cursor: pointer;
+  .header {
+    padding: 8px;
+    border-bottom: 1px solid #ddd;
+    .title {
+      font-weight: 300;
+    }
+    .badage {
+      background: rgba(100, 217, 214, 0.7);
+      padding: 2px;
+      border-radius: 2px;
+      float: right;
+    }
+  }
+  .container {
+    padding: 16px;
+    min-height: 56px;
+    display: flex;
+    justify-content: start;
+    .card-panel-text {
+      line-height: 18px;
+      color: rgba(0, 0, 0, 0.45);
+      font-size: 14px;
+      //   margin-bottom: 12px;
+    }
+
+    .card-panel-num {
+      font-size: 20px;
+    }
+  }
+}
+</style>
