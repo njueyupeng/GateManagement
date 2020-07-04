67114f322aae46828860ed6c4b5ea2fdbf593df6
njueyupeng <1196194875@qq.com>
2020-07-04 21:50:51 +0800

    static-file

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,28 +1,10 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">总览页面</div>
-  </div>
+  <div>name</div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-
-export default {
-  name: "Dashboard",
-  computed: {
-    ...mapGetters(["name"])
-  }
-};
+export default {};
 </script>
 
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
+<style>
 </style>
